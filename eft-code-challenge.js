e466b6d2801825fb780c8a8528a34fddef61f844
piercebelton <piercebelton@gmail.com>
2019-04-12 09:45:15 -0700

    add skeleton account page, and conditional redirects.

--- eft-code-challenge.js
+++ eft-code-challenge.js
@@ -20,3 +20,26 @@ function credentialsValid(email, password){
 function setLoggedInCookie(email){
   document.cookie = "email=" + email;
 }
+
+function redirectIfLoggedIn(link){
+  if (getCookie("email") === "Sincere@april.biz"){
+    window.location.href = "./" + link + ".html";
+  } else {
+    window.location.href = "./index.html";
+  }
+}
+
+function getCookie(cookieName) {
+  var name = cookieName + "=";
+  var cookieArray = document.cookie.split(";");
+  for(var i = 0; i < cookieArray.length; i++) {
+    var cookie = cookieArray[i];
+    while (cookie.charAt(0) == " ") {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(name) == 0) {
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+  return "";
+}
