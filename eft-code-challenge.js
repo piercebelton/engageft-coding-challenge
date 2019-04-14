5a8a1814204c74ab51f74451bd414fb839a93b3b
piercebelton <piercebelton@gmail.com>
2019-04-13 17:50:57 -0700

    add js function to get user data from API, and insert dynamically into account view

--- eft-code-challenge.js
+++ eft-code-challenge.js
@@ -70,10 +70,29 @@ function insertLoginLogoutLink(){
 }
 
 
+function appendUserData(){
+  return $.getJSON('https://jsonplaceholder.typicode.com/users/1', function(data){
+    $("#full-name").append(data.name);
+    $("#company-name").append(data.company.name);
+    $("#company-catchphrase").append(data.company.catchPhrase);
+    $("#company-bs").append(data.company.bs);
+    $("#username").append(data.username);
+    $("#email").append(data.email);
+    $("#phone").append(data.phone);
+    $("#website").append(data.website);
+    $("#street").append(data.address.street);
+    $("#suite").append(data.address.suite);
+    $("#city").append(data.address.city);
+    $("#zip").append(data.address.zipcode);
+  });
+}
+
+
 $( document ).ready(function() {
     insertNavbar();
     insertLoginLogoutLink();
     $("#invalid-alert").hide();
+    appendUserData();
 
     $("#navbar-title").click(function(){
       redirectIfLoggedIn('home');
